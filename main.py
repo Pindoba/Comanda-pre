c86298d6212bbf8f558782eb3c5105581b4b4ac3
Welton Moura <weltonmoura@live.com>
2022-08-01 16:35:21 -0300

    cadastro de produtos e outros bugs

--- main.py
+++ main.py
@@ -10,22 +10,13 @@ import os
 import bcrypt
 import util
 
-
 data = datetime.datetime.now()
 data_str = data.strftime("%d/%m/%y")
 hora = datetime.datetime.now()
 hora_str = hora.strftime("%H:%M:%S")
 data_hora = data_str + " " +hora_str
 
-
 conexao = pymysql.connect(host='DESKTOP-IDQTBUT',user='root', database='banco_dados', password='pindoba10')
-
-# senha = util.criar_senha('123456')
-# print(senha)
-# entrar = str(input('Digite a senha certa:\n'))
-# senhadb = '123456'
-# login_ok = util.verificar_senha(entrar,senha)
-
 
 lista_preco = []
 lista_produto = []
@@ -34,13 +25,14 @@ id_produto = []
 def iniciar():
 
     # nome = 'RAUL ROCK BAR'
-    # banco = sqlite3.connect('banco_dados.db')
+    # banco = sqlite3.connect('config.db')
     # cursor = banco.cursor()
     # cursor.execute(
     #     "CREATE TABLE IF NOT EXISTS comandas (id INTEGER PRIMARY KEY AUTOINCREMENT, numero integer, valor REAL, nome text)")
     # cursor.execute("CREATE TABLE IF NOT EXISTS produtos (id INTEGER PRIMARY KEY AUTOINCREMENT, codigo integer, nome text, valor REAL )")
     # banco.commit()
     # banco.close()
+    listar_produtos()
     
     listar_dados()
     historico()
@@ -52,24 +44,38 @@ def criar_user():
     nivel = user.lineEdit_3.text()
 
 def salvar_user():
+   
     nome =  user.lineEdit.text()
     entrada_senha = str(util.criar_senha(user.lineEdit_2.text())).replace("'","")
     senha = entrada_senha[1:]
-    nivel = user.lineEdit_3.text()
-    print(str(senha))
-    print(type(senha))
-
+    nivel = user.spinBox.text()
+    
     banco = conexao.cursor()
     cursor = conexao.cursor()
-    # cursor.execute(f"INSERT INTO banco_dados.users VALUES (1,'{nome}'), (2,'{senha}'), (3,'{nivel}')")
     cursor.execute("INSERT INTO users (nome, senha, nivel) VALUES ('{}', '{}', '{}')".format(nome,senha,nivel))
-
-    # cursor.execute(f"INSERT INTO banco_dados.users (nome, senha, nivel) VALUES ('{nome}', {senha}, '{nivel}')")
     cursor.execute("commit;")
     banco.close()
+    listar_user()
 
     user.label.setText("Usuário Cadastrado com sucesso!")
     
+def cadastro_produto():
+
+    codigo = user.lineEdit_4.text()
+    nome = user.lineEdit_5.text()
+    preco = user.lineEdit_6.text().replace(",",".")
+
+    banco = conexao.cursor()
+    cursor = conexao.cursor()
+    cursor.execute("INSERT INTO produtos (codigo_produto, nome, valor) VALUES ({}, '{}', {})".format(codigo,nome,preco))
+    cursor.execute("commit;")
+    banco.close()
+    user.label_11.setText('Produto cadastrado com sucesso!')
+    user.lineEdit_4.setText('')
+    user.lineEdit_5.setText('')
+    user.lineEdit_6.setText('')
+    listar_produtos_cadastro()
+
 def add_produto():
     try:
         add.label_11.setText('')
@@ -190,7 +196,7 @@ def add_saldo():
         valor_atual = dados_comanda[0][0]
         nome_db = dados_comanda[0][1]
         nome = guarida.lineEdit_4.text()
-        valor_inserir = float(guarida.lineEdit_2.text())
+        valor_inserir = float(guarida.lineEdit_2.text().replace(",","."))
         valor_atualizado = valor_atual + valor_inserir
         numero_comanda = guarida.lineEdit.text()
 
@@ -225,16 +231,21 @@ def add_saldo():
         listar_dados()
 
 def ler():
-    if guarida.lineEdit.text() != "" or guarida.lineEdit_2.text() != "":
-        banco = conexao.cursor()
-        cursor = conexao.cursor()
-        cursor.execute("SELECT valor, nome FROM comandas WHERE numero_comanda = '"+guarida.lineEdit.text()+"'")
-        dados_comanda = cursor.fetchall()
-        valor = dados_comanda[0][0]
-        nome = dados_comanda[0][1]
-        guarida.label.setText(guarida.lineEdit.text())
-        guarida.label_3.setText(nome)
-        guarida.label_2.setText("R$ "+str(valor))
+    try:
+        if guarida.lineEdit.text() != "" or guarida.lineEdit_2.text() != "":
+            banco = conexao.cursor()
+            cursor = conexao.cursor()
+            cursor.execute("SELECT valor, nome FROM comandas WHERE numero_comanda = '"+guarida.lineEdit.text()+"'")
+            dados_comanda = cursor.fetchall()
+            valor = dados_comanda[0][0]
+            nome = dados_comanda[0][1]
+            guarida.label.setText('Comanda: '+guarida.lineEdit.text())
+            guarida.label_3.setText('Nome: '+nome)
+            guarida.label_2.setText("Valor atual: R$ "+str(valor))
+    except:
+        guarida.label.setText('Comanda não encontrada')
+        guarida.label_3.setText('')
+        guarida.label_2.setText("")
 
 def janela_comanda():
     try:
@@ -249,7 +260,6 @@ def janela_comanda():
         lista_produto.clear()
         add.label_5.setText("R$ 0.00")
         add.label_7.setText("R$ 0.00")
-        # forme.lineEdit_3.setText("")
         add.listWidget.clear()
         add.listWidget_2.clear()
         add.label_6.setText("R$ "+str(dados_comanda[0][0]))
@@ -260,72 +270,64 @@ def janela_comanda():
         print('Comanda não encontrada!')
 
 def confirmar_pedido():
-    if add.lineEdit.text() != "":
-        global lista_preco
-    global lista_produto
-    numero_comanda = forme.lineEdit_3.text()
-    banco = conexao.cursor()
-    cursor = conexao.cursor()
-    cursor.execute("SELECT valor FROM comandas WHERE numero_comanda = '"+numero_comanda+"'")
-    dados_comanda = cursor.fetchall()
-    valor_atual = dados_comanda[0][0]
-    soma = sum(lista_preco)  
-    saldo_final = valor_atual - soma
+    # if add.lineEdit.text() != "":
+        # lista_preco
+        # lista_produto
+        numero_comanda = forme.lineEdit_3.text()
+        banco = conexao.cursor()
+        cursor = conexao.cursor()
+        cursor.execute("SELECT valor FROM comandas WHERE numero_comanda = '"+numero_comanda+"'")
+        dados_comanda = cursor.fetchall()
+        valor_atual = dados_comanda[0][0]
+        soma = sum(lista_preco)  
+        saldo_final = valor_atual - soma
 
-    
-
-    cursor2 = conexao.cursor()
-    cursor2.execute("SELECT nome FROM comandas WHERE numero_comanda = '"+numero_comanda+"'")
-    nome_comanda = cursor2.fetchall()
-    print(id_produto)
-    n = 0
-    for i in id_produto:
-        
-        # if id_produto[n] > 1000 and id_produto[n] < 2000:
+        cursor2 = conexao.cursor()
+        cursor2.execute("SELECT nome FROM comandas WHERE numero_comanda = '"+numero_comanda+"'")
+        nome_comanda = cursor2.fetchall()
+        n = 0
+        for i in id_produto:
             
-            # for j in range(len(id_produto)):
-                cursor = conexao.cursor()
-                cursor.execute("SELECT nome FROM produtos WHERE codigo_produto = '"+str(id_produto[n])+"'")
-                dados_comanda = cursor.fetchall()
+            cursor = conexao.cursor()
+            cursor.execute("SELECT nome FROM produtos WHERE codigo_produto = '"+str(id_produto[n])+"'")
+            dados_comanda = cursor.fetchall()
 
-                print(n)
-                arquivo = open("print.txt", "w")
-                # arquivo.write(str(n))
-                arquivo.write("Comanda: "+numero_comanda)
-                arquivo.write("\n\n"+dados_comanda[0][0]+"\n\n")
-                arquivo.write("\n"+nome_comanda[0][0]+"\n\n")
-                arquivo.write("HORA: "+hora_str)
-                arquivo.write("\nDATA: "+ data_str)
-                arquivo.write("\n          ...")
-                arquivo.close()
+            arquivo = open("print.txt", "w")
+            arquivo.write("Comanda: "+numero_comanda)
+            arquivo.write("\n\n"+dados_comanda[0][0]+"\n\n")
+            arquivo.write(nome_comanda[0][0]+"\n\n")
+            arquivo.write("HORA: "+hora_str)
+            arquivo.write("\nDATA: "+ data_str)
+            arquivo.write("\n          ...")
+            arquivo.close()
 
-                lista_impressoras = win32print.EnumPrinters(2)
-                impressora = lista_impressoras[4]
-                win32print.SetDefaultPrinter(impressora[2])
-                win32api.ShellExecute(0, "print", "print.txt", None, ".", 0)
-                n = n+1        
-                time.sleep(0.4)
-    indice = 0
-    for i in lista_preco:
-        cursor.execute("INSERT INTO historicos (evento, numero_comanda, nome, produto, valor, data) VALUES ('{}', '{}', '{}','{}', {}, '{}')".format("VENDA",forme.lineEdit_3.text(),add.label_10.text(), lista_produto[indice],lista_preco[indice], data_hora))
-        indice +=1
-    historico()
+            lista_impressoras = win32print.EnumPrinters(2)
+            impressora = lista_impressoras[4]
+            win32print.SetDefaultPrinter(impressora[2])
+            win32api.ShellExecute(0, "print", "print.txt", None, ".", 0)
+            n = n+1        
+            time.sleep(0.5)
+        indice = 0
+        for i in lista_preco:
+            cursor.execute("INSERT INTO historicos (evento, numero_comanda, nome, produto, valor, data) VALUES ('{}', '{}', '{}','{}', {}, '{}')".format("VENDA",forme.lineEdit_3.text(),add.label_10.text(), lista_produto[indice],lista_preco[indice], data_hora))
+            indice +=1
+        historico()
 
-    banco = conexao.cursor()
-    cursor = conexao.cursor()
-    cursor.execute(f"UPDATE comandas SET valor = '{saldo_final:.2f}' WHERE numero_comanda = {numero_comanda}")
-    cursor.execute("commit;")
-    banco.close()
-    add.label_5.setText("R$ 0.00")
-    add.label_7.setText("R$ 0.00")
-    lista_preco.clear()
-    lista_produto.clear()
-    id_produto.clear()
-    add.listWidget.clear()
-    add.listWidget_2.clear()
-    forme.lineEdit_3.setText("")
-    add.close()
-    listar_dados()
+        banco = conexao.cursor()
+        cursor = conexao.cursor()
+        cursor.execute(f"UPDATE comandas SET valor = '{saldo_final:.2f}' WHERE numero_comanda = {numero_comanda}")
+        cursor.execute("commit;")
+        banco.close()
+        add.label_5.setText("R$ 0.00")
+        add.label_7.setText("R$ 0.00")
+        lista_preco.clear()
+        lista_produto.clear()
+        id_produto.clear()
+        add.listWidget.clear()
+        add.listWidget_2.clear()
+        forme.lineEdit_3.setText("")
+        add.close()
+        listar_dados()
 
 def cancelar():
     lista_preco.clear()
@@ -361,7 +363,29 @@ def erro():
     cancelar()
 
 def open_user():
-    user.show()
+    nome = forme.lineEdit_9.text()
+    senha = forme.lineEdit_10.text()
+    try:
+        # banco = conexao.cursor()
+        cursor = conexao.cursor()
+        cursor.execute(f"SELECT senha, nome, nivel FROM users WHERE nome = '{nome}'")
+        dados_user = cursor.fetchall()
+        # print(dados_user[0][0])
+
+        senha_verificada = util.verificar_senha(senha, dados_user[0][0])
+        print(senha_verificada)
+
+        if nome == dados_user[0][1] and senha_verificada == True and dados_user[0][2] == '3':
+            forme.label_9.setText("")
+            user.show()
+            listar_user()
+            listar_produtos_cadastro()
+
+        else:
+            forme.label_9.setText("Usuário ou Senha incorreto")
+    except:
+        forme.label_9.setText("Usuário inexistente")
+    # user.show()
 
 def listar_dados():
     banco = conexao.cursor()
@@ -371,7 +395,6 @@ def listar_dados():
     forme.tableWidget.setRowCount(len(dados_lidos))
     forme.tableWidget.setColumnCount(3)
     banco.close()
-    print(dados_lidos)
     
     for i in range(0, len(dados_lidos)):
         for j in range(0, 3):
@@ -381,7 +404,20 @@ def listar_dados():
     for i in range(0, len(dados_lidos)):
         for j in range(1, 2):
             forme.tableWidget.setItem(i, j, QtWidgets.QTableWidgetItem('R$  '+str(dados_lidos[i][j])))
-   
+
+def listar_user():
+        banco = conexao.cursor()
+        cursor = conexao.cursor()
+        cursor.execute("SELECT nome, nivel FROM users")
+        dados_lidos = cursor.fetchall()
+        user.tableWidget.setRowCount(len(dados_lidos))
+        user.tableWidget.setColumnCount(2)
+        banco.close()
+        
+        for i in range(0, len(dados_lidos)):
+            for j in range(0, 2):
+                user.tableWidget.setItem(i, j, QtWidgets.QTableWidgetItem(str(dados_lidos[i][j])))
+
 def historico():
     banco = conexao.cursor()
     cursor = conexao.cursor()
@@ -390,12 +426,51 @@ def historico():
     forme.tableWidget_3.setRowCount(len(dados_lidos))
     forme.tableWidget_3.setColumnCount(6)
     banco.close()
-    print(dados_lidos)
     
     for i in range(0, len(dados_lidos)):
         for j in range(0, 6):
             forme.tableWidget_3.setItem(i, j, QtWidgets.QTableWidgetItem(str(dados_lidos[i][j])))
+    for i in range(0, len(dados_lidos)):
+            for j in range(4, 5):
+                forme.tableWidget_3.setItem(i, j, QtWidgets.QTableWidgetItem('R$  '+str(dados_lidos[i][j])))  
+
+def listar_produtos_cadastro():
+    banco = conexao.cursor()
+    cursor = conexao.cursor()
+    cursor.execute("SELECT * FROM produtos")
+    dados_lidos = cursor.fetchall()
+    user.tableWidget_2.setRowCount(len(dados_lidos))
+    user.tableWidget_2.setColumnCount(3)
+    banco.close()
+    lista_produto()
+    
+    for i in range(0, len(dados_lidos)):
+        for j in range(0, 3):
+            user.tableWidget_2.setItem(i, j, QtWidgets.QTableWidgetItem(str(dados_lidos[i][j])))
             
+    
+    for i in range(0, len(dados_lidos)):
+        for j in range(2, 3):
+            user.tableWidget_2.setItem(i, j, QtWidgets.QTableWidgetItem('R$  '+str(dados_lidos[i][j])))  
+
+def listar_produtos():
+    banco = conexao.cursor()
+    cursor = conexao.cursor()
+    cursor.execute("SELECT * FROM produtos")
+    dados_lidos = cursor.fetchall()
+    forme.tableWidget_2.setRowCount(len(dados_lidos))
+    forme.tableWidget_2.setColumnCount(3)
+    banco.close()
+    # print(dados_lidos)
+    
+    for i in range(0, len(dados_lidos)):
+        for j in range(0, 3):
+            forme.tableWidget_2.setItem(i, j, QtWidgets.QTableWidgetItem(str(dados_lidos[i][j])))
+            
+    
+    for i in range(0, len(dados_lidos)):
+        for j in range(2, 3):
+            forme.tableWidget_2.setItem(i, j, QtWidgets.QTableWidgetItem('R$  '+str(dados_lidos[i][j])))         
 
 # def excluir():
 #     linha = forme.tableWidget.currentRow()
@@ -430,44 +505,7 @@ def historico():
     # editor.lineEdit_3.setText(str(status[0][2]))
 
 
-# def cantou():
-#     banco = sqlite3.connect('banco_dados.db')
-#     linha = forme.tableWidget.currentRow()
-#     cursor = banco.cursor()
-#     cursor.execute("SELECT id FROM dados")
-#     dados_lidos = cursor.fetchall()
-#     valor_id = dados_lidos[linha][0]
 
-#     cursor.execute("SELECT id FROM dados")
-#     valor_id = dados_lidos[linha][0]
-#     cursor.execute("SELECT * FROM dados WHERE id=" + str(valor_id))
-#     status = cursor.fetchall()
-#     banco.close()
-
-
-#     linha = forme.tableWidget.currentRow()
-#     global numero_id
-#     banco = sqlite3.connect('banco_dados.db')
-#     cursor = banco.cursor()
-#     cursor.execute("SELECT id FROM dados")
-#     dados_lidos = cursor.fetchall()
-#     valor_id = dados_lidos[linha][0]
-#     cursor.execute("SELECT * FROM dados WHERE id=" + str(valor_id))
-#     status = cursor.fetchall()
-#     nome = status[0][1]
-#     status_2 = status[0][2]
-#     data = datetime.datetime.now()
-#     data_str = data.strftime("%d/%m/%y")
-#     hora = datetime.datetime.now()
-#     hora_str = hora.strftime("%H:%M")
-#     cursor.execute("UPDATE dados SET status = 'Cantou' WHERE id = " + str(valor_id))
-#     cursor.execute("CREATE TABLE IF NOT EXISTS cantou (id INTEGER PRIMARY KEY AUTOINCREMENT, nome text, status text, data text, hora text)")
-#     cursor.execute("INSERT INTO cantou (nome, status, data, hora) VALUES ('" + nome + "', '" + status_2 + "', '" + data_str + "', '" + hora_str + "')")
-#     cursor.execute("DELETE FROM dados WHERE id=" + str(valor_id))
-#     banco.commit()
-#     forme.tableWidget.removeRow(linha)
-#     listar_dados()
-#     banco.close()
 
 
 # def salvar():
@@ -483,38 +521,6 @@ def historico():
     # banco.close()
     # editor.close()
 
-
-# def hora_atualiza():
-#     global stop
-    
-#     for i in range(99999):
-#         if stop == True:
-#             break
-#         hora = datetime.datetime.now()
-#         hora_str = hora.strftime("%H:%M")
-#         forme.label_4.setText(hora_str)
-#         # print(hora_str)
-#         time.sleep(59)
-
-
-# def histo():
-    # historico.show()
-    # banco = sqlite3.connect('banco_dados.db')
-    # cursor = banco.cursor()
-    # cursor.execute("SELECT * FROM cantou")
-    # # cursor.execute("SELECT * FROM dados WHERE status = ''")
-    # dados_lidos = cursor.fetchall()
-    # historico.tableWidget.setRowCount(len(dados_lidos))
-    # historico.tableWidget.setColumnCount(5)
-    # linha = forme.tableWidget.currentRow()
-
-    # banco.close()
-
-    # for i in range(0, len(dados_lidos)):
-    #     for j in range(0, 5):
-    #         historico.tableWidget.setItem(i, j, QtWidgets.QTableWidgetItem(str(dados_lidos[i][j])))
-
-
 app = QtWidgets.QApplication([])
 forme = uic.loadUi("comanda.ui")
 add = uic.loadUi("add.ui")
@@ -526,19 +532,24 @@ forme.show()
 
 iniciar()
 # threading.Thread(target=hora_atualiza).start()
-
-add.pushButton.clicked.connect(add_produto)
-guarida.pushButton_7.clicked.connect(zerar)
-forme.pushButton_6.clicked.connect(open_user)
-add.pushButton_3.clicked.connect(cancelar)
-forme.pushButton_2.clicked.connect(janela_comanda)
-add.pushButton_2.clicked.connect(confirmar_pedido)
+forme.pushButton_7.clicked.connect(open_user)
 forme.pushButton_4.clicked.connect(entrar_guarida)
+forme.pushButton_2.clicked.connect(janela_comanda)
+forme.pushButton_5.clicked.connect(alterar_nome)
+
+add.pushButton_2.clicked.connect(confirmar_pedido)
+add.pushButton_3.clicked.connect(cancelar)
+add.pushButton.clicked.connect(add_produto)
+
+guarida.pushButton_7.clicked.connect(zerar)
 guarida.pushButton.clicked.connect(add_saldo)
 guarida.pushButton_3.clicked.connect(ler)
+
 ero.pushButton.clicked.connect(erro)
-forme.pushButton_5.clicked.connect(alterar_nome)
+
 user.pushButton.clicked.connect(salvar_user)
+user.pushButton_3.clicked.connect(cadastro_produto)
+
 
 
 app.exec_()
